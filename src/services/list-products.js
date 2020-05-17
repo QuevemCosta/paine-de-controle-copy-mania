c3d1ceb01f0a3310815596c4e743953cea76eb1b
QuevemCosta <quevemcostadacruz@gmail.com>
2020-05-17 00:44:23 -0300

    k

--- src/services/list-products.js
+++ src/services/list-products.js
@@ -1,5 +1,6 @@
 const apiUrl = 'http://localhost:3333/produtcts?'
-const page = 1
+const page = 'page='+1;
+
 
 function listProducts(produtcts){
 	fetch(apiUrl+page)
@@ -7,9 +8,6 @@ function listProducts(produtcts){
 	.then(data => {
 		const produtcts = data.data
 		//console.log(produtcts);
-
-		
-
 
 		//saida em id list-produtos-out
 		produtcts.map(function(product){
@@ -20,9 +18,9 @@ function listProducts(produtcts){
 				parcelas,
 				status,
 				valorAprazo,
-				valorAvista } = product
-			console.log(cod)
-
+				valorAvista 
+			} = product
+			
 		
 			const productEle = document.createElement('li');
 			const imageEle = document.createElement('img');
@@ -30,37 +28,47 @@ function listProducts(produtcts){
 			const valuesEle = document.createElement('span');
 			const grupButonsEle = document.createElement('div');
 			const btnEditEle = document.createElement('button');
-			const btnDeliteEle = document.createElement('button');
 
 
 			const textDescription = document.createTextNode(name);
 			const textValue = document.createTextNode(`A vista ${valorAvista} ou em até ${parcelas} de ${valorAprazo}`);
 			const textBtnEdit = document.createTextNode('Editar');
-			const textBtnDelete = document.createTextNode('Deletar');
 
 			imageEle.setAttribute('src',img);
 			grupButonsEle.setAttribute('class','grup-button');
-
-			btnEditEle.appendChild(textBtnEdit);
-			btnDeliteEle.appendChild(textBtnDelete);
-
-			grupButonsEle.appendChild(btnEditEle);
-			grupButonsEle.appendChild(btnDeliteEle);
+			
+			productEle.setAttribute('id',cod);
+			productEle.setAttribute('onclick',showUrl(cod))
 
 			productEle.appendChild(imageEle);
 			productEle.appendChild(textDescription);
 			productEle.appendChild(textValue)
-			productEle.appendChild(grupButonsEle)
 
 
 
 			document.getElementById('list-produtos-out').appendChild(productEle)		
 
-
 		})
-
-		//console.log(data.pagination)
 	})
 
 }
 listProducts()
+
+function deliteProdutct(cod,name){
+	const deliteUrl = 'http://localhost:3333/produtct/'
+
+	confirm(`Deseja mesmo deletar permanetemento ${name}`)
+
+	fetch(deliteUrl+cod,{
+		method:'DELETE'
+	})
+
+}
+
+function showProduct(cod){
+	const showUrl = 'http://localhost:3333/produtct/'
+
+	fetch(showUrl+cod,{
+		method:'GET',
+	})
+}
